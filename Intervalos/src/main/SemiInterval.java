135f740581627afdf013c93e728f94f80a9c7e72
ANA <ana97gs@gmail.com>
2020-11-29 18:57:45 +0100

    Copiar equals y hashCode y pruebas

--- Intervalos/src/main/SemiInterval.java
+++ Intervalos/src/main/SemiInterval.java
@@ -8,6 +8,31 @@ public class SemiInterval {
 		this.limit = limit;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(limit);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+	    if (obj == null)
+	      return false;
+	    if (getClass() != obj.getClass())
+	      return false;
+	    SemiInterval other = (SemiInterval) obj;
+	    if (Double.doubleToLongBits(limit) != Double.doubleToLongBits(other.limit))
+	      return false;
+	    return true;
+	}
+
+	@Override
 	public String toString() {
 		return this.limit + "";
 	}
--- Intervalos/src/test/test/SemiIntervalTest.java
+++ Intervalos/src/test/test/SemiIntervalTest.java
@@ -16,4 +16,18 @@ public class SemiIntervalTest {
 		assertEquals("5.5", semiInterval.toString());
 	}
 	
+	@Test
+	public void testEqualsSemiInterval() {
+		semiInterval = new SemiInterval(5.5);
+		SemiInterval otherSemiInterval = new SemiInterval(5.5);
+		assertEquals(semiInterval, otherSemiInterval);
+	}
+	
+	@Test
+	public void testHashCode() {
+		semiInterval = new SemiInterval(5.5);
+		SemiInterval otherSemiInterval = new SemiInterval(5.5);
+		assertEquals(semiInterval.hashCode(), otherSemiInterval.hashCode());
+	}
+	
 }
