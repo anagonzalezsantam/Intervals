9217352ffa5808d96acec5fddb77263182745de2
ANA <ana97gs@gmail.com>
2020-11-30 20:22:13 +0100

    Diseñar pruebas para la intersección

--- Intervalos/src/test/test/IntersectionTest.java
+++ Intervalos/src/test/test/IntersectionTest.java
@@ -1,5 +1,6 @@
 package test.test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -16,14 +17,44 @@ public class IntersectionTest {
 	
 	@BeforeEach
 	public void before(){
+		this.fixedInterval = new IntervalBuilder().open(1).open(5).build();
 		this.intervalBuilder = new IntervalBuilder();
-		this.fixedInterval = this.intervalBuilder.open(1).open(5).build();
 	}
 	
 	@Test
-	public void givenTwoLimitsInsideFixedIntervalThenTrue () {
+	public void givenProveIntervalInsideFixedIntervalThenTrue () {
 		this.proveInterval = this.intervalBuilder.open(2).open(4).build();
 		assertTrue(fixedInterval.isIntersected(proveInterval));
+	}
+	
+	@Test
+	public void givenFixedIntervalInsideProveIntervalThenTrue () {
+		this.proveInterval = this.intervalBuilder.open(0).open(6).build();
+		assertTrue(fixedInterval.isIntersected(proveInterval));
+	}
+	
+	@Test
+	public void givenFixedMinInsideProveIntervalThenTrue () {
+		this.proveInterval = this.intervalBuilder.open(0).open(2).build();
+		assertTrue(fixedInterval.isIntersected(proveInterval));
+	}
+	
+	@Test
+	public void givenFixedMaxInsideProveIntervalThenTrue () {
+		this.proveInterval = this.intervalBuilder.open(3).open(6).build();
+		assertTrue(fixedInterval.isIntersected(proveInterval));
+	}
+	
+	@Test
+	public void givenProveIntervalLowerThanFixedIntervalThenFalse () {
+		this.proveInterval = this.intervalBuilder.open(0).open(0.5).build();
+		assertFalse(fixedInterval.isIntersected(proveInterval));
+	}
+	
+	@Test
+	public void givenProveIntervalGreaterThanFixedIntervalThenFalse () {
+		this.proveInterval = this.intervalBuilder.open(6.5).open(7).build();
+		assertFalse(fixedInterval.isIntersected(proveInterval));
 	}
 	
 }
