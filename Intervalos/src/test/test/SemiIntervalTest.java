870dc70e73ba47a23a19cdbd3d78d7c2e7133cf9
ANA <ana97gs@gmail.com>
2020-11-30 19:25:03 +0100

    SemiIntervalTest abstract class

--- Intervalos/src/test/test/SemiIntervalTest.java
+++ Intervalos/src/test/test/SemiIntervalTest.java
@@ -2,45 +2,25 @@ package test.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import main.OpenLowerSemiInterval;
 import main.OpenUpperSemiInterval;
 import main.SemiInterval;
+import test.Point;
 
-public class SemiIntervalTest {
+public abstract class SemiIntervalTest {
 
-	public SemiInterval semiInterval;
+	protected SemiInterval semiInterval;
+	protected Point point;
 	
-	@Test
-	public void testCreateSemiInterval() {
-		semiInterval = new OpenLowerSemiInterval(5.5);
-		assertEquals("5.5)", semiInterval.toString());
-		semiInterval = new OpenUpperSemiInterval(5.5);
-		assertEquals("(5.5", semiInterval.toString());
-	}
+	@BeforeEach
+	  public void before(){
+	    this.point = new Point(4.4);
+	    this.semiInterval = this.createSemiInterval();
+	  }
 	
-	@Test
-	public void testEqualsSemiInterval() {
-		semiInterval = new OpenLowerSemiInterval(5.5);
-		SemiInterval otherSemiInterval = new OpenLowerSemiInterval(5.5);
-		assertEquals(semiInterval, otherSemiInterval);
+	public abstract SemiInterval createSemiInterval();
 		
-		semiInterval = new OpenUpperSemiInterval(5.5);
-		otherSemiInterval = new OpenUpperSemiInterval(5.5);
-		assertEquals(semiInterval, otherSemiInterval);
-	}
-	
-	@Test
-	public void testHashCode() {
-		semiInterval = new OpenLowerSemiInterval(5.5);
-		SemiInterval otherSemiInterval = new OpenLowerSemiInterval(5.5);
-		assertEquals(semiInterval.hashCode(), otherSemiInterval.hashCode());
-		
-		semiInterval = new OpenUpperSemiInterval(5.5);
-		otherSemiInterval = new OpenUpperSemiInterval(5.5);
-		assertEquals(semiInterval.hashCode(), otherSemiInterval.hashCode());
-	}
-	
-	
 }
